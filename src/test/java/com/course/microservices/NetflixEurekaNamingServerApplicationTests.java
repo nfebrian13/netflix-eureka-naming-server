07b7600664d4df36b02d1f5a3d2f6f496f051d2b
nfebrian13 <nanaz.febrian@gmail.com>
2023-03-11 11:39:50 +0700

    Initial commit

---
+++ src/test/java/com/course/microservices/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
